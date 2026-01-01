27c155806238746d66cdfe3cdc9af2d72eb52156
adinda17 <adinda.aulia2000@gmail.com>
2026-01-01 09:35:12 +0700

    Update QA projects

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,16 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/CV_Adinda Aulia Balqis.pdf";
+import pdf from "../../Assets/CV_Adinda Aulia Balqis.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -19,7 +21,8 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
+
+        <Row style={{ justifyContent: "center", marginBottom: "30px" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -32,22 +35,27 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+  <Document
+    file={pdf}
+    onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+  >
+    {Array.from(new Array(numPages), (el, index) => (
+      <div
+        key={`page_${index + 1}`}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "30px"
+        }}
+      >
+        <Page
+          pageNumber={index + 1}
+          scale={width > 786 ? 1.7 : 0.6}
+        />
+      </div>
+    ))}
+  </Document>
+</Row>
       </Container>
     </div>
   );
