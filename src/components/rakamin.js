d705a7127705093afc5054fa198986b2f4743b0a
adinda17 <adinda.aulia2000@gmail.com>
2026-04-19 10:24:44 +0700

    final ppt

--- src/components/rakamin.js
+++ src/components/rakamin.js
@@ -6,7 +6,7 @@ function Rakamin() {
     <QAPPTTemplate
       title="KlinikGo Health System Analysis"
 subtitle="Project-Based Internship | Rakamin Academy"
-pptLink={`${process.env.PUBLIC_URL}/RakaminPpt.pdf`}
+pptLink="https://adinda17.github.io/RakaminPpt.pdf"
 backLink="/project"
 summary={
   <>
